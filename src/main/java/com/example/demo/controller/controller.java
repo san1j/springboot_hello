4bf2ae19b15eb852c6d4211bb8df8e5fb5ac2c85
san1j <sanjanasreenath96@gmail.com>
2018-05-19 17:17:25 -0400

    use thymeleaf

--- src/main/java/com/example/demo/controller/controller.java
+++ src/main/java/com/example/demo/controller/controller.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
   
 @Controller
 public class controller {
+	
+	@Value("${welcome.message}")
+	String welcomeMessage;
 
   @RequestMapping("/")
-   public String index() {
+   public String index(Model model) {
+	model.addAttribute("message", welcomeMessage);
     return "hello";
   }
 
